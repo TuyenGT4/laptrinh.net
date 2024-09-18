2ef0c0a73c8cc780ae412ecb4736234419e69cf4
TuyenGT4 <139259606+TuyenGT4@users.noreply.github.com>
2024-09-18 22:23:19 +0700

    Update testSanPham.cs

--- testSanPham.cs
+++ testSanPham.cs
@@ -2,7 +2,7 @@ using System;
 namespace BaiTap_KeThua;
 class testSP{
     static void Main(string[] args){
-        SanPham sp1 = new SanPham("Quang", 5000, "Con zin", 1);
+        SanPham sp1 = new SanPham("Quang", 5000, "", 1);
         sp1.hienThi();
 }
 }
