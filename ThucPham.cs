8487492500d96121945d1f3078b0d3498aee33a1
TuyenGT4 <139259606+TuyenGT4@users.noreply.github.com>
2024-10-23 09:29:41 +0700

    Delete ThucPham.cs

--- ThucPham.cs
+++
@@ -1,9 +0,0 @@
-
-namespace BaiTap_KeThua;
-class ThucPham : SanPham{
-    private DateTime ExpirationDate { get; set; }
-    public ThucPham(string tenSanPham, float gia, string moTa, int soLuong, DateTime expirationDate) : base(tenSanPham, gia, moTa, soLuong)
-    {
-        this.ExpirationDate = expirationDate;
-    }
-}
